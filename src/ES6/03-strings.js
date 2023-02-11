0ee431f2df2458e1da77c3fa33acb48e02200ee2
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-02-10 22:36:17 -0600

    strings

---
+++ src/ES6/03-strings.js
@@ -0,0 +1,17 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world;
+
+// template literals
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+// multiline strings
+let lorem = "Esto es un string \n esto es otra linea";
+
+let lorem2 = `Esta es una frase epica
+esto es otra linea`;
+
+console.log(lorem);
+console.log(lorem2);
