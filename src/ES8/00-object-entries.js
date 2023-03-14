de34d904a2812cc796a7271c64699db806a2a865
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-03-14 07:25:12 -0600

    ES8: string padding y trailing commas

---
+++ src/ES8/00-object-entries.js
@@ -0,0 +1,10 @@
+const countries = {
+	MX: "Mexico",
+	US: "United States",
+	CA: "Canada",
+	CO: "Colombia",
+	CL: "Chile",
+	GT: "Guatemala",
+};
+
+console.log(Object.entries(countries));
---
+++ src/ES8/04-trailing-commas.js
@@ -0,0 +1,5 @@
+const age = [24, 34, 23, 14, 18, , , ,];
+
+console.log(age);
+
+console.log(age.length);
